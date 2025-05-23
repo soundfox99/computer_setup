7e797a8e52229216f0c329474d3c6762348d567b
soundfox99 <66957702+soundfox99@users.noreply.github.com>
2025-05-23 18:08:01 -0400

    Updated lua plugins for neovim for lsp and autocompletion

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,29 +1,33 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-        }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_insall = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,34 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = { "BufReadPost", "BufNewFIle" },
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "bash",
-                "python",
-                "javascript",
-                "typescript",
-                "html",
-                "css",
-                "json",
-                "yaml",
-                "markdown",
-                "go",
-                "dockerfile",
-                "c",
-                "cpp",
-                "java",
-                "sql",
-                "make",
-                "toml",
-                "regex",
-                "rust"
-            },
-            indent = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "CR>",
-                    node_decremental = "<BS>",
-                    scope_incremental = "<TAB>",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                },
-            },
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFIle" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "CR>",
+          node_decremental = "<BS>",
+          scope_incremental = "<TAB>",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+      },
+    })
+  end,
 }
