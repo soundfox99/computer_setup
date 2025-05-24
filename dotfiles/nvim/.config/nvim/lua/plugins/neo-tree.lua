a9a6f86e7b9651849b8c57df3c6b3192917a3cc7
soundfox99 <66957702+soundfox99@users.noreply.github.com>
2025-05-24 08:43:41 -0400

    Updated neotree to show hideen files by default

--- dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,21 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+		},
+	},
+
+	vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {}),
 }
