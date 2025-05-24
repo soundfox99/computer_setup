06c02987c492f8d0b0a75a67ee06a7fa3d7d58ba
soundfox99 <66957702+soundfox99@users.noreply.github.com>
2025-05-24 08:42:43 -0400

    Made it so nvim completions won't by default select the first choise when you hit enter

--- dotfiles/nvim/.config/nvim/lua/plugins/completions.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/completions.lua
@@ -34,7 +34,7 @@ return {
          ["<C-f>"] = cmp.mapping.scroll_docs(4),
          ["<C-Space>"] = cmp.mapping.complete(),
          ["<C-e>"] = cmp.mapping.abort(),
-         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- item. Set 'select' to 'false' to only cofirm explicitly selected
+         ["<CR>"] = cmp.mapping.confirm({ select = false }), -- item. Set 'select' to 'false' to only cofirm explicitly selected
        }),
        sources = cmp.config.sources({
         -- { name = "nvim_lsp" },
